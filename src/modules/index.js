544bc8ef9dc4d7b94e81d9cd34d5e9b0d841d057
Stephen Zuniga <stephen.zuniga001@gmail.com>
2017-01-15 18:36:18 -0500

    cleanup unnecessary code

--- src/modules/index.js
+++ src/modules/index.js
@@ -3,14 +3,6 @@ import Duration from 'duration-js';
 // Get the previous, current and next levels
 export function getLevelsForDisplay(levels, currentLevel) {
 
-  // if (currentLevel === 0) {
-  //   return {
-  //     previous: undefined,
-  //     current: undefined,
-  //     next: undefined
-  //   };
-  // }
-
   return {
     previous: levels[currentLevel - 1],
     current: levels[currentLevel],
